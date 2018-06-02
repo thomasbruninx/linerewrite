1403e19038413ed67d2f966766c6ac630441f567
Thomas Bruninx <thomasbruninx@users.noreply.github.com>
2018-06-02 02:40:42 +0200

    Fixed include <cstring>
    fixed include <string> => <cstring>

--- linerewrite.cpp
+++ linerewrite.cpp
@@ -15,7 +15,7 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #include <vector>
-#include <string>
+#include <cstring>
 #include <iostream>
 #include <fstream>
 #include <exception>
